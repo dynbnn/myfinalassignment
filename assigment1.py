a192451420923cf2281bef3a9e27aef4d2a70ff2
dyn bnn <85924318+dynbnn@users.noreply.github.com>
2022-06-08 16:51:12 +0800

    Update assigment1.py

--- assigment1.py
+++ assigment1.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
@@ -27,9 +26,9 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-iris = datasets.load_iris()
-X = iris.data
-Y = iris.target
+iris=pd.read_csv(https://raw.githubusercontent.com/dynbnn/myfinalassignment/main/IRIS.csv)
+X = iris.[:,:-1]
+Y = iris.[:,-1]
 
 clf = RandomForestClassifier()
 clf.fit(X, Y)
